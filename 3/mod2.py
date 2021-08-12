0cde9f2c6607dc0ac9fe2ba77f7468a0f61576b3
VanLance <87987880+VanLance@users.noreply.github.com>
2021-08-12 09:08:56 -0500

    Add files via upload

---
+++ 3/mod2.py
@@ -0,0 +1,25 @@
+
+
+def form():
+    length = int(input("what is the length of your house?: "))
+    width = int(input("what is the width of your house?: "))
+    area = length * width
+    return area
+
+# answer = form()
+
+
+
+
+
+def  circum():
+    radius = int(input("What is the radius of your circle?: "))
+    circumfrence = 2 * 3.15 * radius
+    return circumfrence
+
+# answertwo =circum()
+
+
+# print(answer)
+# print(answertwo)
+
